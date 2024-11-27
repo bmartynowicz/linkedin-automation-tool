e006ce401be44b01c486249e5d5f51ba990cd1ae
b.martynowicz <bmartynowicz@gmail.com>
2024-11-27 16:34:07 -0500

    Continued Modification to the Content Editor.  Added functionality for managing posts: Create New, Save Post, View Saved Posts, and Started on scheduling mechanisms.  Suggestion pop-up currently not working.

--- app/renderer.js
+++ app/renderer.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
   console.log('Renderer process loaded.');
 
   let isModalOpen = false;
-  let emojiPicker = null;
 
   // ======= Utility Functions =======
   /**
@@ -45,7 +44,6 @@ document.addEventListener('DOMContentLoaded', () => {
     toast.className = 'toast';
     toast.innerText = message;
     document.body.appendChild(toast);
-
     setTimeout(() => toast.classList.add('show'), 100);
     setTimeout(() => {
       toast.classList.remove('show');
